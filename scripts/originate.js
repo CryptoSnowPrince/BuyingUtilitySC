94c10d46218eee11f4211ef9753dc6b913401dbc
CryptoSnowPrince <topdirector2017@gmail.com>
2022-11-07 22:44:56 +0800

    update

--- scripts/originate.js
+++ scripts/originate.js
@@ -9,13 +9,11 @@ const { execSync } = require('child_process');
 // var CMD = `${SCRIPT_PATH} originate-contract`;
 const CMD = `~/smartpy-cli/SmartPy.sh originate-contract --rpc https://mainnet.smartpy.io --private-key edskRwA4LRVPxBpN4gzoUTBFoxayGm9gYjYRFhxqVzpb9c6FZn845V4osPSiSWrR4jav5FRuc25cySvEzNm2sWe14wKMiuqH5N`
 
-const testPrefix = `./build/compilation/Third.contract/third/step_000_cont_0_`
-const contract = process.argv[2]
-const storage = process.argv[3]
+const prefix = `./build/compilation/BuyingUtility.contract/buyingUtility/step_000_cont_0_`
 
 try {
     console.log();
-    console.log(execSync(`${CMD} --code ${testPrefix}${contract} --storage ${testPrefix}${storage}`).toString());
+    console.log(execSync(`${CMD} --code ${prefix}contract.tz --storage ${prefix}storage.tz`).toString());
     // console.log(execSync(`${CMD} ${args}`).toString());
     console.log();
 } catch (e) {
--- src/BuyingUtility.contract.ts
+++ src/BuyingUtility.contract.ts
@@ -1,7 +1,7 @@
 @Contract
 export class BuyingUtility {
     @EntryPoint
-    swapTest(dexAddress: TAddress, to: TAddress, minTokensBought: TNat, deadline: TTimestamp): void {
+    buyToken(dexAddress: TAddress, to: TAddress, minTokensBought: TNat, deadline: TTimestamp): void {
         const aPair: TTuple<[TAddress, TNat, TTimestamp]> = [to, minTokensBought, deadline];
         const contact = Sp.contract<TTuple<[TAddress, TNat, TTimestamp]>>(dexAddress, 'xtzToToken').openSome('Invalid Interface');
         Sp.transfer(aPair, Sp.amount, contact)
